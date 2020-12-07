51d752573d25decfa8db5585a2b2f214ebd701aa
Rahul Sheth <rahul.sheth@snapchat.com>
2020-12-07 13:02:49 -0500

    Update Hunter and dependency versions

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,2 +1,5 @@
 hunter_config(ZLIB VERSION 1.2.8-p3
               CMAKE_ARGS CMAKE_POSITION_INDEPENDENT_CODE=TRUE)
+
+hunter_config(abseil-cpp VERSION 20200923.2)
+hunter_config(Protobuf VERSION 3.14.0-4a09d77-p0)
